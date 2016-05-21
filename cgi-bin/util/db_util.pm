2748eb6d0bcc6929ce8cc5d1de53a39df2c93a1f
Luca Boscolo <bosluca93@gamil.com>
2016-05-21 11:00:23 +0200

    Funzioni per XML
    Aggiunte:
    §getFilename
    §getFilenameUtenti §caricamentoLibXML §caricamentoLibXMLUtenti

--- cgi-bin/util/db_util.pm
+++ cgi-bin/util/db_util.pm
@@ -1,4 +1,46 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+use CGI::Carp qw(warningsToBrowser fatalsToBrowser);#DA TOGLIERE PRIMA DELLA CONSEGNA SERVE A VEDERE GLI ERRORI DA BROWSER
+use CGI::Session;
+use XML::LibXML;
+use Encode qw(encode);
+
+use Exporter qw(import);
+our @EXPORT = qw(getFilename getFilenameUtenti caricamentoLibXML caricamentoLibXMLUtenti );
+
+package util::db_util;
+
+#---------------- PRELEVA IL FILE PREZZI.XML -----------------
+sub getFilename{
+    return "../data/prezzi.xml";
+}
+
+#---------------- PARSA IL FILE RESTITUITO DALLA SUB getFilename() ---------------
+sub caricamentoLibXML{
+
+    my $filename = getFilename(); # ATTENZIONE path Windows
+
+    my $parser = XML::LibXML->new();
+
+    return $parser->parse_file($filename);
+
+}
+
+#--------------- PRELEVA IL FILE UTENTI.XML --------------------
+sub getFilenameUtenti{
+  return "../data/utenti.xml"
+}
+
+#---------------- PARSA IL FILE RESTITUITO DALLA SUB getFilenameUtenti() ---------------
+sub caricamentoLibXMLUtenti{
+  my $filename = getFilenameUtenti();
+
+  my parser = XML::LibXML->new();
+
+  return $parser->parse_file($filename);
+}
 
 
 #Questo file contiene subroutine per LETTURA E SCRITTURA del DATABASE in XML
