951723b9c048cd0172bafe929fae1e7b136fa1aa
ThomasFuser <fuser.thomas@gmail.com>
2016-05-28 16:38:59 +0200

    add & fix mod e eliminazione

--- cgi-bin/riepilogoModificaAbbonamento.cgi
+++ cgi-bin/riepilogoModificaAbbonamento.cgi
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # use module
 use util::html_util;
-use util::html_content;
+use util::html_content_admin;
 use CGI::Carp qw(warningsToBrowser fatalsToBrowser);
 use CGI;
 use CGI::Session;
@@ -10,5 +10,8 @@ use CGI::Session;
 
 
 util::html_util::start_html("Riepilogo modifica abbonamento");
+
 util::html_content_admin::stampaRiepilogoModifica();
+
+
 util::html_util::end_html();
--- cgi-bin/util/html_content_admin.pm
+++ cgi-bin/util/html_content_admin.pm
@@ -6,11 +6,12 @@ use CGI::Carp qw(warningsToBrowser fatalsToBrowser);#DA TOGLIERE PRIMA DELLA CON
 use CGI::Session;
 use XML::LibXML;
 use util::db_util;
+use util::html_content;
 use Encode qw(encode);
 
 
 use Exporter qw(import);
-our @EXPORT = qw(enc stampaIndex stampaPrezzi stampaPrezziAcquistabili stampaStaff stampaCross stampaSoft stampaCardio);
+our @EXPORT = qw(stampaRiepilogoModifica stampaPrezziAcquistabili deleteAbbonamento);
 
 package util::html_content_admin;
 
@@ -20,16 +21,13 @@ package util::html_content_admin;
 
 sub stampaRiepilogoModifica{
    
-
     my $cgi = CGI->new(); # create new CGI object
-
-    #id abbonamento da modificare
-    my $id_abbonamento= "002";
 
     # Dati recuperati dalle form
     my $Descrizione_form = $cgi->param('descrizione');
     my $Durata_form = $cgi->param('periodo');
     my $Prezzo_form = $cgi->param('prezzo');
+    my $id_abbonamento= $cgi->param('modifica_Abbonamento');
 
     #sostituzione dei caratteri '<' e '>' con caratteri safe che non danno problemi nel database xml
     my $sostMinore="&lt;";
@@ -70,7 +68,7 @@ sub stampaRiepilogoModifica{
     #modifica del campo descrizione
     util::db_util::modifica( $padre, $PathDescrizione, $nuovaDescrizione, $parser);    #nodo padre, nodo da sostituire, nuovo tag, parser
 
-    #salvataggio delle modifiche
+    #salvataggio delle modifiche 
     open(OUT,">$file") or die;
     print OUT $doc->toString;
     close(OUT);
@@ -85,18 +83,98 @@ sub stampaRiepilogoModifica{
 
     util::html_content::stampaPacchetto($area, $Descrizione_form,$Durata_form, $Prezzo_form);   
     print "
-         <p id=\"ritorno\"> Ritorna alla pagina <a href=\"prezzi.cgi\" > Prezzi </a> </p>   
+         <p id=\"ritorno\"> Ritorna alla pagina <a href=\"prezziModificabili.cgi\" > Prezzi </a> </p>   
     ";
 
     print " </div> ";
-    
+}
 
+#--------------- PAGINA PREZZI DA STAMPARE QUANDO SI E' UN AMMINISTRATORE ---------------
+sub stampaPrezziAcquistabili{
+
+    #my $q = new CGI;
+    #my ($user,$path)= @_;
+    my $valuta = "€";
+    my $doc = util::db_util::caricamentoLibXML();
+
+    my $query = "listaAbbonamenti/categoria";
+
+
+    print "
+    <div id=\"content\">
+        <h1>Prezzi e Offerte</h1>
+    ";
+
+
+    foreach my $titArea($doc->findnodes($query))
+    {
+        my $area = util::html_content::enc($titArea->findnodes("./titolo"));
+         ($area) = ($area =~ /<titolo>(.*)<\/titolo>/);
+
+         print"
+         <div class=\"packages\"><h2> $area </h2> ";
+
+        foreach my $partAbb($titArea->findnodes("./abbonamento"))
+        {
+            my $durata = util::html_content::enc($partAbb->findnodes("./durata"));
+            ($durata)=($durata=~ /<durata>(.*)<\/durata>/);
+
+            my $prezzo = util::html_content::enc($partAbb->findnodes("./prezzo"));
+            ($prezzo)=($prezzo=~ /<prezzo>(.*)<\/prezzo>/);
+
+            my $desc = util::html_content::enc($partAbb->findnodes("./descrizione"));
+            ($desc)=($desc=~ /<descrizione>(.*)<\/descrizione>/);
+
+            my $id = util::html_content::enc($partAbb->getAttribute('ID'));  #recupero dell'id selezionato
+            
+            print"
+            <ul class=\"package\">
+            <li class=\"title\"> $durata </li>
+            <li class=\"price\"> $prezzo $valuta </li>
+            <li class=\"description\"> $desc </li>
+            <li> <form class=\"description\" action=\"ModificaAbbonamento.cgi\" method=\"post\">
+           <button name=\"Mod\" type=\"submit\" class=\"submit_button\" value=\"$id\" >Modifica</button>
+</form>  </li>
+<li> <form class=\"description\" action=\"eliminazione.cgi\" method=\"post\">
+           <button name=\"Elim\" type=\"submit\" class=\"submit_button\" value=\"$id\" >Elimina</button>
+</form>  </li>
+            </ul>
+            
+            ";
+
+            }
+        print "</div>";
+    }# CHIUSURA FOREACH TITOLI
+
+    print "</div>";
+
+}
+
+sub deleteAbbonamento{
+
+    my $cgi = new CGI;
+    my $file = "../data/prezzi.xml";
+    my $parser = XML::LibXML->new();
+    my $doc = $parser->parse_file($file);
+
+    #recupero dell'id dal bottone
+    my $id_abbonamento = $cgi->param('Elim');
+
+    #eliminazione del pacchetto
+    my $nodoElim = $doc->findnodes("//abbonamento[\@ID=$id_abbonamento]")->get_node(1);
+    my $parent = $nodoElim->parentNode;
+    $parent->removeChild($nodoElim);
+
+    #salvataggio delle modifiche nel database
+    open(OUT,">$file") or die $!;
+    print OUT $doc->toString;
+    close(OUT);      
+
+    #redirect alla pagina "prezziModificabili"   
+    print "Location:prezziModificabili.cgi\n\n";
 }
 
 
 
-
-
-
-
 1;
+
